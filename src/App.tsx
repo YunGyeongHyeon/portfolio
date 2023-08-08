97ca127f32b89d326f4bf107741fba34bec2a360
윤경현 <yungyeonghyeon@yungyeonghyeon-ui-MacBookAir.local>
2023-08-08 16:53:08 +0900

    first setting commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            Hello React! <br/>
+            What the fuck is that!!!!
+
+        </div>
+    )
+}
+
+export default App;
